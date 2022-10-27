4b4caa02489e33c6d12fcfa5dabaf17c44a767ec
0lucus <63613363+0lucus@users.noreply.github.com>
2022-10-27 22:53:04 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,27 @@
+document.querySelector(".days").addEventListener("keyup", update);
+
+function update() {
+  const days = document.querySelector(".days").value;
+  fetch(`http://api.nbp.pl/api/cenyzlota/last/${days}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const goldData = data
+        .map(
+          (element, index) =>
+            `<div style="height: ${element.cena}px; ${
+              data[index - 1]
+                ? element.cena > data[index - 1].cena
+                  ? 'background-color: green;"'
+                  : 'background-color: red;"'
+                : '"'
+            } ></div>`
+        )
+        .join("");
+      const goldDays = data.reverse().map((element) => element.data);
+      const goldPrice = data.reverse().map((element) => element.cena);
+      document.querySelector(".gold-chart").innerHTML = goldData;
+      document.querySelector(".value").innerHTML = `<b>${
+        goldPrice[0]
+      }$</b> <br> ${goldDays[goldDays.length - 1]}`;
+    });
+}
